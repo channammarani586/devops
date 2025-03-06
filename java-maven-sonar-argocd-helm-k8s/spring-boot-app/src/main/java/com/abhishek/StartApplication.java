198aa63bc2f9ff05c8222a5131c761623ed6fc12
channammarani586 <sbiradarchannamma@gmail.com>
2025-03-06 15:24:43 +0530

    Update StartApplication.java

--- java-maven-sonar-argocd-helm-k8s/spring-boot-app/src/main/java/com/abhishek/StartApplication.java
+++ java-maven-sonar-argocd-helm-k8s/spring-boot-app/src/main/java/com/abhishek/StartApplication.java
@@ -1,27 +1,11 @@
 package com.abhishek;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@RestController
-@RequestMapping("/")
-public class AppController {
-
-    @GetMapping
-    public String getPodDetails() {
-        String podIp = "";
-        String nodeIp = System.getenv("KUBERNETES_NODE_IP"); // Get Node IP from environment
-
-        try {
-            podIp = InetAddress.getLocalHost().getHostAddress(); // Get Pod IP
-        } catch (UnknownHostException e) {
-            podIp = "Unknown";
-        }
-
-        return "<h1>Pod IP: " + podIp + "</h1>\n" +
-               "<h1>Node IP: " + (nodeIp != null ? nodeIp : "Unknown") + "</h1>";
+@SpringBootApplication
+public class StartApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StartApplication.class, args);
     }
 }
