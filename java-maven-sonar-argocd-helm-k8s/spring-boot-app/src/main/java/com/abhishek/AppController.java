f87ac24b27f1d399321790c5d0c2cdb25683ade3
channammarani586 <sbiradarchannamma@gmail.com>
2025-03-06 15:25:21 +0530

    Create AppController.java

---
+++ java-maven-sonar-argocd-helm-k8s/spring-boot-app/src/main/java/com/abhishek/AppController.java
@@ -0,0 +1,27 @@
+package com.abhishek;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@RestController
+@RequestMapping("/")
+public class AppController {
+
+    @GetMapping
+    public String getPodDetails() {
+        String podIp = "";
+        String nodeIp = System.getenv("KUBERNETES_NODE_IP"); // Get Node IP from environment
+
+        try {
+            podIp = InetAddress.getLocalHost().getHostAddress(); // Get Pod IP
+        } catch (UnknownHostException e) {
+            podIp = "Unknown";
+        }
+
+        return "<h1>Pod IP: " + podIp + "</h1>\n" +
+               "<h1>Node IP: " + (nodeIp != null ? nodeIp : "Unknown") + "</h1>";
+    }
+}
